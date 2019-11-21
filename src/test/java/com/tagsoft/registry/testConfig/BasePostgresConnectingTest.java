dfb4fd78e19e4c6d2e098d94d2783bbc13549e6d
Ivan <jezhische@gmail.com>
2019-11-21 08:17:45 +0200

    adjusting test module, adding BasePostgresConnectingTest

---
+++ src/test/java/com/tagsoft/registry/testConfig/BasePostgresConnectingTest.java
@@ -0,0 +1,32 @@
+package com.tagsoft.registry.testConfig;
+
+import com.tagsoft.registry.RegistryApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+// need either @ContextConfiguration(classes = TextsaverApplication.class),
+// or
+ @SpringBootTest(classes = {RegistryApplication.class})
+//, or @DataJpaTest
+
+// When I use this profile, I get "...'entityManagerFactory': Requested bean is currently in creation: Is there an
+// unresolvable circular reference?". So I need to use @Profile and @ActiveProfiles also with the production configuration.
+//@ActiveProfiles("testPostgres")
+
+// https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-testing.html
+//  "By default, @DataJpaTest configures an in-memory embedded database, scans for @Entity classes, and configures
+// Spring Data JPA repositories".
+//@DataJpaTest
+// "By default, data JPA tests are transactional and roll back at the end of each test.  If that is not what you want,
+// you can disable transaction management for a test or for the whole class as follows:"
+@Transactional/*(propagation = Propagation.NOT_SUPPORTED)*/
+@Rollback(value = false)
+// " If, however, you prefer to run tests against a real database...:"
+@AutoConfigureTestDatabase(/*connection = EmbeddedDatabaseConnection.NONE, */replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BasePostgresConnectingTest {
+}
